8772d469a89e793ce66be396ec152d29abae0364
joseunghyo <seunghyo7742@naver.com>
2022-04-27 02:15:51 +0900

    clean function

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import Button from './Button';
 import styles from './App.module.css';
 import { useState, useEffect } from 'react';
+
+function Hello(){
+  function destroyedFn(){
+    console.log("Bye!");
+  }
+  function effectFn(){
+    console.log("created")
+    return destroyedFn; //component가 파괴될 때, return한 function 실행
+  }
+  
+  useEffect(effectFn,[]);
+    
+  return <h1>Hello</h1>
+}
+
 function App() {
-  const [counter, setValue] = useState(0); //useState가 배열 반환 , counter의 초기값=0
-  const onClick = () => setValue((prev) => prev + 1);
-  const [keyword, setKeyword] = useState('');
-  const onChange = (event) => setKeyword(event.target.value); //argument =event, value를 setKeyword로
-
-  useEffect(() => {
-    console.log('I run only once.');
-  }, []);
-
-  useEffect(() => {
-    if (keyword !== '' && keyword.length > 6)
-      //5이 상의 입력이 들어와야 실행됨
-      console.log('I run when keyworkd changed', keyword); //counter 말고 keyword가 변할 때만 리렌더 되도록 하고싶음
-  }, [keyword]); //keyword가 변화할때만 코드를 실행해줘. 빈 array일 때는 한번만 실행됨
-
-  useEffect(() => {
-    if(counter!==0)
-      console.log('I run when counter changed', counter);
-  }, [counter]);
-  return (
-    <div>
-      <input
-        value={keyword}
-        onChange={onChange} //event Listener
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-    </div>
-  );
+  const [showing, setShowing]= useState(false);
+  const onClick =()=> setShowing((prev)=>!prev);
+  return <div>
+    {showing ? <Hello/>: null} //자바스크립트 쓸때는 중괄호, Component는 단지 jsx를 부루는 function
+    <button onClick={onClick}>{showing ? "Hide":"Show"}</button>
+  </div>;
 }
 
 export default App;
