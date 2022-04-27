c13c2f7750886e001b5c5dfef58c8d51c52649c2
joseunghyo <seunghyo7742@naver.com>
2022-04-27 16:19:06 +0900

    detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,28 @@
 import { getMouseEventOptions } from '@testing-library/user-event/dist/utils';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail() {
   const { id } = useParams(); //react router의 변수 값을 넘겨 받음
-  
+  const [movie, setMovie] = useState([]);
   const getMovie = async () => {
-      const json = await (
-        await fetch(`https://yts.mx/api/v2/movie-details.json?movie_id=${id}`)
-      ).json();
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+
+    setMovie(json.data.movie);
+
   };
+ 
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      <img src={movie.large_cover_image}/>
+      <h1>{movie.title}</h1>
+    </div>
+  );
 }
 export default Detail;
