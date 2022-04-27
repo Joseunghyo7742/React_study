0715c6397b189fa9450e016b98964b5b7e5e9aad
joseunghyo <seunghyo7742@naver.com>
2022-04-27 09:33:25 +0900

    toDolist2

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
     }
 
     setToDo(''); //empty the input
-    setToDos((array) => [toDo, ...array]); //현재 Array를 받아옴 --> toDo와 기존 어레이 합침
+    setToDos((array) => [toDo, ...array]); //현재 Array를 받아옴 --> toDo와 기존 어레이 합침 // 리액트는 함수의 첫번째 argument로 현재 state를 보냄
     console.log(toDos);
   };
 
   return (
     <div>
-      <h1>My ToDos</h1>
+      <h1>My ToDos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -29,8 +29,14 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item,index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
-  );
+  ); //map은 하나의 배열에 있는 item을 내가 원하는 무엇이든지로 바꿔주는 역할, 새로운 배열로 리턴.
 }
 
 export default App;
